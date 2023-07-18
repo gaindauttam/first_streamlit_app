9693fd12c1e81af838a4841edb35d6078d1264f5
gaindauttam <139829883+gaindauttam@users.noreply.github.com>
2023-07-18 14:02:43 +1200

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit;
 streamlit.title("My Parents' new healthy diner");
-streamlit.header("Breakfast Menu");
-streamlit.text("Omega 3 and Blueberry Oatmeal");
-streamlit.text("Hard-Boiled Free Range Egg");
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast');
 
+   
