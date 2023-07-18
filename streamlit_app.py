5d11499e4ef1fd48914890cfc06199f7b6ff09f0
gaindauttam <139829883+gaindauttam@users.noreply.github.com>
2023-07-18 13:59:27 +1200

    Update streamlit_app.py
    Added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit;
-streamlit.title("This is my first streamlist app");
+streamlit.title("My Parents' new healthy diner");
+streamlit.header("Breakfast Menu");
+streamlit.text("Omega 3 and Blueberry Oatmeal");
+streamlit.text("Hard-Boiled Free Range Egg");
+
